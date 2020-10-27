d08496c6941bdb26e3fee69ccae9250718fd15a2
ivanariasastua <69368731+ivanariasastua@users.noreply.github.com>
2020-10-26 18:57:32 -0600

    adiccion de entidades

---
+++ examen/src/main/java/org/una/examen/entities/Distrito.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.examen.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author Ivan Josué Arias Astua
+ */
+@Entity
+@Table(name = "exa_iaa_distritos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Distrito implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(length = 50)
+    private String nombre;
+    
+    @Column(length = 15)
+    private String codigo;
+    
+    @Column
+    private Integer poblacion;
+    
+    @Column
+    private Float area;
+    
+    @ManyToOne 
+    @JoinColumn(name="canton")
+    private Canton canton;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "distrito")
+    private List<Unidad> unidades;
+}
---
+++ examen/src/main/java/org/una/examen/entities/Provincia.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.examen.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author Ivan Josué Arias Astua
+ */
+@Entity
+@Table(name = "exa_iaa_provincias")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Provincia implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(length = 50)
+    private String nombre;
+    
+    @Column(length = 15)
+    private String codigo;
+    
+    @Column
+    private Integer poblacion;
+    
+    @Column
+    private Float area;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "provincia")
+    private List<Canton> cantones;
+}
